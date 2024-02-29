c04f8c3a43a31f3f47be7ebd16123518d9f81784
Syrgak <85344623+syrgak33@users.noreply.github.com>
2024-03-01 01:32:17 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,25 +14,25 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"  # Change this to your desired availability zone
-  map_public_ip_on_launch = true
+#resource "aws_subnet" "public_subnet" {
+#  vpc_id                  = aws_vpc.my_vpc.id
+#  cidr_block              = "10.0.1.0/24"
+#  availability_zone       = "us-east-1a"  # Change this to your desired availability zone
+#  map_public_ip_on_launch = true
+#
+#  tags = {
+#    Name = "public-subnet"
+#  }
+#}
 
-  tags = {
-    Name = "public-subnet"
-  }
-}
+#resource "aws_internet_gateway" "my_igw" {
+ # vpc_id = aws_vpc.my_vpc.id
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "my-igw"
-  }
-}
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc
-}
+#  tags = {
+#    Name = "my-igw"
+#  }
+#}#
+#
+#resource "aws_route_table" "public_route_table" {
+#  vpc_id = aws_vpc.my_vpc
+#}
