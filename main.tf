0f9ea8bfeb9c3cad6ae5e300774ca7331a3c538b
Syrgak <85344623+syrgak33@users.noreply.github.com>
2024-03-01 01:34:44 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 # main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+}
 
 provider "aws" {
   region = "us-east-1"  # Change this to your desired AWS region
